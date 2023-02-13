660ce2f921bba538557368f254fe8b49f1fe64ae
Fuck the Error! <74085442+lizzy-km@users.noreply.github.com>
2023-02-13 23:16:24 +0630

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Left from './left'
+import Right from './right'
+import Top from './top'
+import Mid from './mid'
 
-function App() {
+const App = () => {
+  // const blackpink = [
+  //   {
+  //     id: 1,
+  //     username: "Lalisa",
+  //     description:
+  //       "Detail",
+  //     image: "https://i.pinimg.com/474x/86/ad/8c/86ad8ca08bef251166cf6e0ff9c5f913.jpg",
+     
+  //   },
+  //   {
+  //     id: 2,
+  //     username: "Kim Jisoo",
+  //     description:
+  //       "Detail",
+  //     image:
+  //       "https://i.pinimg.com/474x/29/8a/2a/298a2a26ee41eb8d864c0bddd14218b2.jpg",
+  //   },
+  //   {
+  //     id: 3,
+  //     username: "Park Chaeyoung",
+  //     description:
+  //       "Detail",
+  //     image:
+  //       "https://i.pinimg.com/474x/69/8f/41/698f416c08feb77049fcedbfe9b6de70.jpg",
+  //   },
+  //   {
+  //     id: 4,
+  //     username: "Jennie Kim",
+  //     description:
+  //       "Detail",
+  //     image:
+  //       "https://i.pinimg.com/474x/86/81/20/86812015336442c38a2b6e086125c8de.jpg",
+  //   },
+   
+  // ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <main>
+    
+    <Top/>
+    <section id="section">
+             
+             
+  <Left/>    <Mid/>   <Right/>
+    </section>
+    
+  </main>
   );
-}
+};
 
 export default App;
